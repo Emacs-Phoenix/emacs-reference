0df56ab0654314d301f978310c3562a735eae169
AbyChan <crystal.tyan@gmail.com>
2016-09-03 23:18:42 +0800

    add some data

---
+++ data/dired/entering-and-exitiing-dired.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export default {
+  name: 'Entering and Exiting Dired',
+  type: 'data-list',
+  data: [{
+    desc: 'run dired',
+    shortcut: 'C-x d'
+  }, {
+    desc: 'dired the directory of the file you are editing',
+    shortcut: 'C-x C-j'
+  }] 
+}
